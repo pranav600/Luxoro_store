829ae23d74a205de7c86b73d1846fa250ab1d9d9
pranav600 <pranavjoshitkd@gmail.com>
2025-07-12 12:25:28 +0530

    added favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,50 @@
-"use client";
+// "use client";
 
+// import "./globals.css";
+// import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
+// import { AuthProvider } from "../context/auth-context";
+
+// import { usePathname } from "next/navigation";
+// import { Geist, Geist_Mono } from "next/font/google";
+
+// const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const pathname = usePathname();
+//   const isAdmin = pathname.startsWith("/admin"); // 👈 check if it's admin page
+
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black min-h-screen`}
+//       >
+//         <AuthProvider>
+//           {!isAdmin && <Navbar />}
+//           {children}
+//           {!isAdmin && <Footer />}
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
+"use client"
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { AuthProvider } from "../context/auth-context";
 
-import { usePathname } from "next/navigation";
 import { Geist, Geist_Mono } from "next/font/google";
+import { usePathname } from "next/navigation";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
@@ -20,10 +58,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  const isAdmin = pathname.startsWith("/admin"); // 👈 check if it's admin page
+  const isAdmin = pathname.startsWith("/admin");
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Luxoro</title>
+        {/* Optional meta tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black min-h-screen`}
       >
